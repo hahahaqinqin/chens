858f569ff56a7d6dad4082128ee806a2758d191c
Dante_Von_Armia <dantestylish@hotmail.com>
2017-05-23 15:03:26 +0800

    rebuild gh-pages

--- README.md
+++
@@ -1,3 +0,0 @@
-# chens
-Restaurant Portal Website
-[view demo](https://dante-vonarmia.github.io/chens/)
--- app/scripts/main.js
+++
@@ -1,5 +0,0 @@
-// Home swiper init
-var swiper = new Swiper('.swiper-container.home', {
-	pagination: '.swiper-pagination',
-	paginationClickable: true
-});
