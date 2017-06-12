418f626bbd94cfe265df1ddddb1ebb2822bab983
Dante_Von_Armia <dantestylish@hotmail.com>
2017-06-12 11:30:58 +0800

    nav active status

--- routes/views/contact.js
+++ routes/views/contact.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', function(req, res, next) {
 	res.render('contact', {
 		title: 'Mr Chen\'s Contact',
-		result: sampleFunc.sampleFunc(999)
+		curl: req.originalUrl
 	});
 });
 
--- routes/views/restaurant.js
+++ routes/views/restaurant.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('restaurant', { title: 'Mr Chen\'s Restaurant' });
+	res.render('restaurant', {
+		title: 'Mr Chen\'s Restaurant',
+		curl: req.originalUrl
+	});
 });
 
 
